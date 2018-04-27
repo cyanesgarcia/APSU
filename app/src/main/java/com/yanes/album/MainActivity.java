85c01695277a5ca7762abbdf3b70e1e7b566b190
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-04-27 14:06:31 -0500

    layout- main activity

--- app/src/main/java/com/yanes/album/MainActivity.java
+++ app/src/main/java/com/yanes/album/MainActivity.java
@@ -2,12 +2,32 @@ package com.yanes.album;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button album = (Button)findViewById(R.id.album);
+        album.setOnClickListener(this);
+        Button about_us= (Button) findViewById(R.id.about_us);
+        about_us.setOnClickListener(this);
+        Button resource= (Button) findViewById(R.id.resources);
+        resource.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.album){
+
+        }else if(view.getId() == R.id.about_us){
+
+        }else if(view.getId() == R.id.resources){
+
+        }
+
     }
 }
