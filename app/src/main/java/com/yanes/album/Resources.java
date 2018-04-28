08eeac8cf0a4a53b12031d9c1aaa941f8dddd4e1
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-04-28 04:28:26 +0200

    Update Resource class

--- app/src/main/java/com/yanes/album/Resources.java
+++ app/src/main/java/com/yanes/album/Resources.java
@@ -1,8 +1,13 @@
 package com.yanes.album;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by LidiayClaudia on 28/04/2018.
  */
 
-public class Resources {
-}
+public class Resources extends Activity {
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+}}
