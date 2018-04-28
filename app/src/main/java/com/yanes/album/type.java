3be096ce8cca821cd03d88271d19616b2a94faf7
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-04-28 04:20:55 +0200

    Link the Type class with the Info

--- app/src/main/java/com/yanes/album/type.java
+++ app/src/main/java/com/yanes/album/type.java
@@ -1,7 +1,10 @@
 package com.yanes.album;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -9,7 +12,7 @@ import android.widget.ListView;
  * Created by LidiayClaudia on 28/04/2018.
  */
 
-public class type extends Activity {
+public class type extends Activity implements AdapterView.OnItemClickListener {
     public static String Type;
     static final String[] TYPES = {
             "Capital", "Foods & Drinks", "Places",
@@ -25,5 +28,14 @@ public class type extends Activity {
                 this, android.R.layout.simple_list_item_1, TYPES
         );
         lv.setAdapter(adapter1);
+        lv.setOnItemClickListener(this);
+    }
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        Type = (String) adapterView.getItemAtPosition(i);
+
+        Intent intent=new Intent(this,Info.class);
+        startActivity(intent);
     }
 }
