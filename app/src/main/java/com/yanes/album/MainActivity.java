ee7105e71ce8b9178781c60ddde255a29d0b2a57
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-04-29 21:21:43 +0200

    Update toolbar

--- app/src/main/java/com/yanes/album/MainActivity.java
+++ app/src/main/java/com/yanes/album/MainActivity.java
@@ -5,13 +5,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity implements View.OnClickListener{
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
         Button album = (Button)findViewById(R.id.album);
         album.setOnClickListener(this);
         Button about_us= (Button) findViewById(R.id.about_us);
