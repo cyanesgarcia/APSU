2c4a0c4d5cfc115cdbfaa3b38e2a17cdb26062cf
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-04-29 11:46:26 -0500

    Update

--- app/src/main/java/com/yanes/album/MainActivity.java
+++ app/src/main/java/com/yanes/album/MainActivity.java
@@ -19,7 +19,6 @@ int total =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         //toolbar=(Toolbar) findViewById(R.id.toolbar);
