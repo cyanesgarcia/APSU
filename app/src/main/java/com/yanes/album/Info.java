10243a2998df6e9f48a61defd79d9fb9d2dbd818
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-04-29 23:38:15 -0500

    Update cards bought

--- app/src/main/java/com/yanes/album/Info.java
+++ app/src/main/java/com/yanes/album/Info.java
@@ -3,18 +3,14 @@ package com.yanes.album;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
-import android.content.res.*;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ThemedSpinnerAdapter;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,6 +21,7 @@ public class Info extends ListActivity {
     private DBDataSource dbDataSource;
     private AlertDialog dialog;
     private int card_key= 100;
+    private Album fin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +48,21 @@ public class Info extends ListActivity {
         super.onListItemClick(l, v, position, id);
 
         dialog = new AlertDialog.Builder(this).create();
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
         ArrayAdapter adapter = (ArrayAdapter<Album>) getListAdapter();
-        final Album fin = (Album) adapter.getItem(position);
+        fin=(Album)adapter.getItem(position);
+
+
+        for(int ii=0; ii<MainActivity.check.size();ii++){
+            if(fin.getName().equals(MainActivity.check.get(ii))){
+                card_key=1;
+                break;
+            }else{
+                card_key=100;
+            }
+        }
+
+        if(card_key!=1){
 
         dialog.setTitle("Do you want to buy this card?");
         dialog.setButton("Yes", new DialogInterface.OnClickListener(){
@@ -61,41 +70,18 @@ public class Info extends ListActivity {
 
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-for(int ii=0; ii<MainActivity.check.size();ii++){
-    if(fin.getName().equals(MainActivity.check.get(ii))){
-        card_key=1;
-        break;
-    }else{
-        card_key=100;
-    }
-}
 
-                     if(MainActivity.total>=5 || card_key==1){
-    if(card_key!=1) {
 
+                     if(MainActivity.total>=5 ){
 
         MainActivity.check.add(fin.getName());
         MainActivity.total -= 5;
-    }else{
-        card_key=100;
+        card_key = 1;
     }
 
-                             builder.setMessage(Html.fromHtml("<html>" + "<p><b>Name: </b>" + fin.getName()+"</p>" + "<p><b>State: </b>" + fin.getState()+"</p>" +
-                                     "<p><b>Type: </b>" + fin.getType()+"</p>" + "<p><b>Description: </b>" + fin.getDescription() +"</p>" +"</html>"));
-                             builder. setTitle("Info");
-                             builder.create().show();
+f();
+                 }});
 
-                     }else{
-                         Toast.makeText(getApplicationContext(),"You do not have enough coins",Toast.LENGTH_LONG).show();
-                             builder.setMessage(Html.fromHtml("<html>" + "<p><b>Name: ????</b>" +"</p>" + "<p><b>State: ????</b>" + "</p>" +
-                                     "<p><b>Type: ????</b>"+"</p>" + "<p><b>Description: ????</b>" +"</p>" +"</html>"));
-                             builder. setTitle("Info");
-                             builder.create().show();
-                         }
-                 }
-
-
-                });
                 dialog.setButton2("No",new DialogInterface.OnClickListener(){
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
@@ -107,9 +93,33 @@ for(int ii=0; ii<MainActivity.check.size();ii++){
 
 
 
+    }else if(card_key ==1 ){
+            f();
+    }
+        card_key=100;
+    }
+    public void f(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        if(card_key==1) {
+
+
+        builder.setMessage(Html.fromHtml("<html>" + "<p><b>Name: </b>" + fin.getName()+"</p>" + "<p><b>State: </b>" + fin.getState()+"</p>" +
+                "<p><b>Type: </b>" + fin.getType()+"</p>" + "<p><b>Description: </b>" + fin.getDescription() +"</p>" +"</html>"));
+        builder. setTitle("Info");
+        builder.create().show();
+
+    }else{
+        Toast.makeText(getApplicationContext(),"You do not have enough coins",Toast.LENGTH_LONG).show();
+        builder.setMessage(Html.fromHtml("<html>" + "<p><b>Name: ????</b>" +"</p>" + "<p><b>State: ????</b>" + "</p>" +
+                "<p><b>Type: ????</b>"+"</p>" + "<p><b>Description: ????</b>" +"</p>" +"</html>"));
+        builder. setTitle("Info");
+        builder.create().show();
+    }
+
     }
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
         dbDataSource.close();
     }
