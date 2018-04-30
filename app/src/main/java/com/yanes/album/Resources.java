2c4848fb312c75166bd937dca7030d9fa9a9181d
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-04-30 10:11:12 +0200

    update uri

--- app/src/main/java/com/yanes/album/Resources.java
+++ app/src/main/java/com/yanes/album/Resources.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * Created by LidiayClaudia on 28/04/2018.
@@ -13,8 +14,13 @@ public class Resources extends Activity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resources);
+        TextView te= (TextView)findViewById(R.id.CA);
+        String hola= goToUrl("https://matadornetwork.com");
+        te.setText(hola);
 
-        Uri uri= Uri.parse("https://matadornetwork.com");
+}public String goToUrl(String st){
+        Uri uri= Uri.parse(st);
         Intent intent= new Intent(Intent.ACTION_VIEW, uri);
         startActivity(intent);
-}}
+        return st;
+    }}
