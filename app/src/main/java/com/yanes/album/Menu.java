fb6a91b53d62d2f7eae0b50990fb6f1737ecf989
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-06-21 13:13:05 +0000

    class menu

---
+++ app/src/main/java/com/yanes/album/Menu.java
@@ -0,0 +1,45 @@
+package com.yanes.album;
+
+
+    import android.app.Activity;
+    import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+    public class Menu extends Activity {
+
+        private TextView infoTextView;
+        private BottomNavigationView bottomNavigationView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            infoTextView = (TextView) findViewById(R.id.infoTextView);
+
+            bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    if (item.getItemId() == R.id.inicioItem) {
+                        infoTextView.setText(R.string.inicio);
+                    } else if (item.getItemId() == R.id.buscarItem) {
+                        infoTextView.setText(R.string.buscar);
+                    } else if (item.getItemId() == R.id.favoritosItem) {
+                        infoTextView.setText(R.string.favoritos);
+                    } else if (item.getItemId() == R.id.perfilItem) {
+                        infoTextView.setText(R.string.perfil);
+                    }
+
+                    return true;
+                }
+            });
+        }
+    }
+
