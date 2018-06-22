be87a90a41952f16425977560f7cf2974cbfc9fb
lyanesgarcia <lyanesgarcia@my.apsu.edu>
2018-06-22 09:41:42 -0500

    lateral menu

--- app/src/main/java/com/yanes/album/MainActivity.java
+++ app/src/main/java/com/yanes/album/MainActivity.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,28 +19,37 @@ import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity implements View.OnClickListener,NavigationView.OnNavigationItemSelectedListener {
     private static final int REQUEST_CODE_ADD=100;
     public static String Activity_KEY ="activity";
     public static ArrayList<String> check = new ArrayList<>();
     public static ArrayList<View> position = new ArrayList<>();
     public static ArrayList<Integer> po = new ArrayList<>();
-Toolbar toolbar;
+android.support.v7.widget.Toolbar toolbar;
 public static int total =0;
 
     @Override
     protected void onResume() {
-        toolbar.setTitle(total+" coins");
+       // toolbar.setTitle(total+" coins");
         super.onResume();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
+        NavigationView n =(NavigationView)findViewById(R.id.nav_view);
+        n.setItemIconTintList(null);
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+       // toolbar.setLogo(R.drawable.coin);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.drawable.coin);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
         Button album = (Button)findViewById(R.id.album);
         album.setOnClickListener(this);
@@ -71,6 +85,27 @@ public static int total =0;
     }
 
     }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_album) {
+            // Handle the camera action
+        } else if (id == R.id.nav_classifications) {
+
+        } else if (id == R.id.nav_resources) {
+
+        } else if (id == R.id.nav_aboutus) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -89,13 +124,19 @@ public static int total =0;
                 total+=h;
 
 
-                toolbar.setTitle(total+ " coins");
+              //  toolbar.setTitle(total+ " coins");
             }
         }
     }
 
     @Override
     public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
 
     }
 }
--- app/src/main/java/com/yanes/album/Menu.java
+++ app/src/main/java/com/yanes/album/Menu.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.menu);
 
             infoTextView = (TextView) findViewById(R.id.infoTextView);
 
