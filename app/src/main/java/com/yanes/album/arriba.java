6792626fbfd34df126b8b39a485816482c3dbe0f
cyanesgarcia <cyanesgarcia@my.apsu.edu>
2018-06-28 14:35:45 +0000

    listview layout

--- app/src/main/java/com/yanes/album/arriba.java
+++ app/src/main/java/com/yanes/album/arriba.java
@@ -2,6 +2,7 @@ package com.yanes.album;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class arriba extends Activity {
@@ -13,6 +14,7 @@ public class arriba extends Activity {
 
          }
    public void setUsername(String user_name){
+        Log.i("holeee"," leee");
        TextView textView1=(TextView) findViewById(R.id.name_header);
        textView1.setText(user_name);
 
